197e1081fcb638446a08f6ee3ca2142014b1ad90
peter <97747863+wcorn@users.noreply.github.com>
2023-01-03 21:28:30 +0900

    singleton

---
+++ src/designpattern/singleton/Aclazz.java
@@ -0,0 +1,13 @@
+package designpattern.singleton;
+
+public class Aclazz {
+    private SocketClient socketClient;
+
+    public Aclazz() {
+        this.socketClient = SocketClient.getInstance();
+    }
+
+    public SocketClient getSocketClient() {
+        return this.socketClient;
+    }
+}
---
+++ src/designpattern/singleton/Main.java
@@ -0,0 +1,14 @@
+package designpattern.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Aclazz aclazz = new Aclazz();
+        BClazz bClazz = new BClazz();
+        SocketClient aClient = aclazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
+        //aClient와 bClient는 모두 SocketClient를 할당 받은 aclazz와 bclazz를 각각 생성하였지만
+        //SocketClient는 static으로 생성되어 data 영역에 할당되어 프로그램 시작과 동시에 생성되고 시작하여 자원을 공유한다.
+        //따라서 객체를 여러개 만들어도 각 객체가 가지고 있는 SocketClient는 공유되는 객체이다.
+        System.out.println(aClient.equals(bClient));
+    }
+}
