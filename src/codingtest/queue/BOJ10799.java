7f89cce39d6a7fb3c96e3b5b06610ca6b5dea9ba
peter <97747863+wcorn@users.noreply.github.com>
2023-07-19 10:12:53 +0900

    BOJ 10799 (stack) solve

---
+++ src/codingtest/queue/BOJ10799.java
@@ -0,0 +1,31 @@
+package codingtest.queue;
+
+import java.io.*;
+
+public class BOJ10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] bracket = br.readLine().toCharArray();
+        int count = 0;
+        int result = 0;
+        for (int i = 0; i < bracket.length; i++) {
+            if (bracket[i] == ')') {
+                result++;
+                count--;
+            }
+            if (bracket[i] == '(') {
+                if (i + 1 < bracket.length) {
+                    if (bracket[i + 1] == ')') {
+                        result += count;
+                        i++;
+                    } else {
+                        count++;
+                    }
+                } else {
+                    count++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
