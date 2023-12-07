7237a36ec240f1c6e162db46942a4d052e329f7e
peter <97747863+wcorn@users.noreply.github.com>
2023-12-07 23:34:39 +0900

    [BOJ] 최솟값 만들기 - 레벨2/정렬

--- src/codingtest/math/BOJ1806.java
+++ src/codingtest/math/BOJ1806.java
@@ -8,6 +8,7 @@ public class BOJ1806 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        Collections.reverseOrder();
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
