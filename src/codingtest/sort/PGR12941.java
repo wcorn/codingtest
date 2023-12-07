7571b224353db3ad7b1d8a40604675332db5cea2
peter <97747863+wcorn@users.noreply.github.com>
2023-12-07 23:35:37 +0900

    [BOJ] 최솟값 만들기 - 레벨2/정렬

---
+++ src/codingtest/sort/PGR12941.java
@@ -0,0 +1,21 @@
+package codingtest.sort;
+
+import java.util.Arrays;
+
+public class PGR12941 {
+    public static void main(String[] args) {
+        int[] A={1,2};
+        int[] B={3,4};
+        System.out.println(solution(A, B));
+    }
+    public static int solution(int[] A, int[] B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i=0;i<A.length;i++) {
+            answer+=(A[i]*B[A.length-1-i]);
+        }
+        return answer;
+    }
+}
