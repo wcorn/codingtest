819382d2b93778b04b4b1a223f2819ba2d93918a
wcorn <dsk08208@gmail.com>
2024-02-22 23:48:12 +0900

    [BOJ] 직사각형 - Bronze5/math

---
+++ src/codingtest/math/BOJ27323.java
@@ -0,0 +1,10 @@
+package codingtest.math;
+
+import java.io.*;
+
+public class BOJ27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(Integer.parseInt(br.readLine())*Integer.parseInt(br.readLine()));
+    }
+}
