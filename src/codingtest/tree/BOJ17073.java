a401ef8340d65844c5b1a7836a06c67e0ecc9847
wcorn <dsk08208@gmail.com>
2024-03-08 09:43:53 +0900

    [BOJ] 나무 위의 빗물 - Gold4/Math

--- src/codingtest/tree/BOJ17073.java
+++ src/codingtest/tree/BOJ17073.java
@@ -10,7 +10,7 @@ public class BOJ17073 {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         double water = Double.parseDouble(st.nextToken());
-        int count=0;
+        int count = 0;
         int[] nodes = new int[N + 1];
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -18,10 +18,10 @@ public class BOJ17073 {
             nodes[Integer.parseInt(st.nextToken())]++;
         }
         for (int i = 2; i <= N; i++) {
-            if(nodes[i]<2){
+            if (nodes[i] < 2) {
                 count++;
             }
         }
-        System.out.printf("%.10f",water/count);
+        System.out.printf("%.10f", water / count);
     }
 }
