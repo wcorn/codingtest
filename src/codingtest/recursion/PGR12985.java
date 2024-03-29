68537aa43aed94e2213ca723e06afa5b1651acd1
wcorn <dsk08208@gmail.com>
2024-03-29 10:10:22 +0900

    [PGR] 예상 대진표 - lv2/recursion

---
+++ src/codingtest/recursion/PGR12985.java
@@ -0,0 +1,17 @@
+package codingtest.recursion;
+
+public class PGR12985 {
+    public static void main(String[] args) {
+        System.out.println(solution(8,4,7));
+    }
+    public static int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+        return answer;
+    }
+}
